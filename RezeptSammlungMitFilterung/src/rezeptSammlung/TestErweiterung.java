d48fc861cbfb23764b78f3bfd22ca8dfa3d84e2c
HermannTobolka <hermtgit@aon.at>
2015-11-22 19:32:41 +0100

    NeueTestklasseErstelle

---
+++ RezeptSammlungMitFilterung/src/rezeptSammlung/TestErweiterung.java
@@ -0,0 +1,9 @@
+package rezeptSammlung;
+
+public class TestErweiterung {
+
+	public static void main(String[] args) {
+System.out.println("Kuckuck, ich bin eine Erweiterung des Projekts");
+	}
+
+}
